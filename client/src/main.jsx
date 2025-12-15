1ee00da8adecc0a120f280404e2b81200f2f8178
ndukwopeace <ndukwopeace@gmail.com>
2025-12-15 23:03:56 +0100

    changed vercel.json

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import {BrowserRouter, HashRouter} from 'react-router-dom'; // Import BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <HashRouter>
+
+      <App />
+          </HashRouter>
+
   </StrictMode>,
 )
